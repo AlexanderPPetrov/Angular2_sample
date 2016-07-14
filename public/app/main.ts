b673be8a85a54d076b83243c631e0a87e190cde2
alexanderpetrov <alexander.petrov@novarto.com>
2016-07-14 09:48:37 +0300

    structure change

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,3 +1,3 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+import { Menu } from './menu/menu.component';
+bootstrap(Menu);
